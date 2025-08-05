323f6fd8e3901dbacf0d577e81a1ac13859a3585
Violent788 <ritamuoneke166@gmail.com>
2025-08-05 20:46:01 +0100

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,19 @@
+{
+  "name": "violence-lover-bot",
+  "version": "1.0.0",
+  "description": "A sassy, unpredictable chat bot with personality, mood switching, cooldowns, and memory.",
+  "main": "src/index.js",
+  "scripts": {
+    "start": "node src/index.js",
+    "dev": "nodemon src/index.js"
+  },
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "dotenv": "^16.0.0",
+    "discord.js": "^14.0.0"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.0"
+  }
+    }
